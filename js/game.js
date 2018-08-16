9690de7bbefc9c6213e028e2ea7d48e5503f9958
Deepak Mehta <dmehta@Ingenuity.local>
2018-08-16 12:11:10 +0530

    added logs

--- js/game.js
+++ js/game.js
@@ -3,37 +3,57 @@ new Vue({
   data: {
 		playerHealth: 100,
 		monsterHealth: 100,
-		gameIsRunning: false
+		gameIsRunning: false,
+		gameLogs : []
   },
 	methods: {
-
 		startGame: function(){
 			this.gameIsRunning = true;
 			this.playerHealth = 100;
 			this.monsterHealth = 100;
+			this.gameLogs = [];
 		},
 
 		attack: function(){
 			var damage = this.calculateDamage(3,10);
 			this.monsterHealth -= damage;
+			this.gameLogs.push('PLAYER HITS MONSTER FOR '+ damage)
+
 			if(this.checkWin()){
 				return;
 			}
 			damage = this.calculateDamage(5,12);
-			this.playerHealth -= damage;
+			this.playerHealth -= damage;		
+			this.gameLogs.push('MONSTER HITS PLAYER FOR '+ damage)
 			this.checkWin();
 		},
 
 		specialAttack: function(){
+			var damage = this.calculateDamage(7,15);
+			this.monsterHealth -= damage;
+			this.gameLogs.push('PLAYER HITS MONSTER FOR '+ damage)
+			if(this.checkWin()){
+				return;
+			}
 
+			damage = this.calculateDamage(3,10);
+			this.playerHealth -= damage;
+			this.gameLogs.push('MONSTER HITS PLAYER FOR '+ damage)
+			this.checkWin();
 		},
 
 		heal: function(){
-
+			if(this.playerHealth <= 90){
+				this.playerHealth += 10;
+			}else{
+				this.playerHealth = 100;
+			}
 		},
 
 		giveUp: function(){
-
+			this.gameIsRunning = false;
+			this.playerHealth = 100;
+			this.monsterHealth = 100;
 		},
 
 		calculateDamage: function(min, max){
@@ -42,18 +62,18 @@ new Vue({
 
 		checkWin: function(){
 			if(this.monsterHealth <= 0){
-					if(confirm('You won! New Game?'))if(this.monsterHealth <= 0){
+					if(confirm('You won! New Game?')){
 							this.startGame();
-					} else {
-							this.gameIsRunning = false;
-					}
+						} else {
+								this.gameIsRunning = false;
+						}
 					return true;
 			}else if (this.playerHealth <= 0){
-					if(confirm('You lost! New Game?'))if(this.monsterHealth <= 0){
-							this.startGame();
-					} else {
-							this.gameIsRunning = false;
-					}
+					if(confirm('You lost! New Game?')){	
+								this.startGame();
+						} else {
+								this.gameIsRunning = false;
+						}
 					return true;
 			}
 			return false;
